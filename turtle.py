58ab4103c20b131f6281714a33e0c19ccb7be2ce
Abdulmajeed <132199458+mj95d@users.noreply.github.com>
2024-01-28 00:58:38 +1100

    Create turtle.py
    Create special logo design

---
+++ turtle.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+bgcolor("black")
+color("lime")
+pensize(2)
+right(45)
+speed(11)
+
+
+
+
+for i in range(150):
+    if 7 < i < 62:
+        left
+        if 80 < i < 133:
+            right(5)
+            circle(30)
+            if i < 80:
+                forward(10)
+            else:
+                forward(5)
+
+
+while True:
+    Turtle.update()
